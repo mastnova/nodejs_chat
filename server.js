20d8fbd8bb8d8b4bfc224ad68dddf2e629827a9f
Denis Matveev <mast747@bk.ru>
2016-03-07 23:19:47 +0300

    Add chat history

--- server.js
+++ server.js
@@ -5,26 +5,32 @@ const socket = require('socket.io');
 
 const server = express();
 const serverSocket = socket.listen(server.listen(8080));
+let msgLimit = 20,
+		messages =[],
+		users = [];
 
 server.set('views', __dirname + '/tpl');
 server.set('view engine', 'jade');
 server.engine('jade', require('jade').__express);
 server.use(express.static('public'));
 
-let users = [];
-
 server.get('/', function (req, res) {
 	res.render('page');
 })
 
 serverSocket.sockets.on('connection', function (client) {
-	client.emit('message', 'Welcome to chat!');
+	client.emit('message', messages.join('<br>'));
+	client.emit('users', users);
 
 	client.on('send', function (data) {
 		client.get('nickname', function (err, nickname) {
-			serverSocket.sockets.emit('message', nickname + ': ' + data);
+			let message = nickname + ': ' + data;
+			if (messages.length == msgLimit) {
+				messages.shift();
+			}
+			messages.push(message)
+			serverSocket.sockets.emit('message', message);
 		})
-
 	});
 
 	client.on('nickname', function (nickname) {
